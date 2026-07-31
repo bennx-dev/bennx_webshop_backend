c3141fa1c903a9ea79d0b529c326a8218da42387
user <user@email.com>
2026-07-31 19:11:59 +0200

    Disable CSRF and add /api/contact to the permitted list.

--- src/main/java/dev/webshop/config/Security.java
+++ src/main/java/dev/webshop/config/Security.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -55,12 +56,14 @@ public class Security {
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(c -> c
                         .requestMatchers("/login"
-                        ,"/api/categorieen/**"
-                        ,"/api/artikelen/**").permitAll()
+                                ,"/api/categorieen/**"
+                                ,"/api/artikelen/**"
+                                , "/api/contact/**").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .formLogin(AbstractHttpConfigurer::disable);
 
 
         return http.build();
